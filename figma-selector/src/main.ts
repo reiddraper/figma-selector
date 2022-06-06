28b979e0c6f5d3d7714a480c7445c75dbb03eec7
Reid Draper <reiddraper@gmail.com>
2022-06-06 17:35:02 -0500

    Basic working prototype

--- figma-selector/src/main.ts
+++ figma-selector/src/main.ts
@@ -1,3 +1,11 @@
+import {
+  CssSelectorParser,
+  Rule,
+  RuleAttr,
+  RuleSet,
+  Selector,
+} from "css-selector-parser";
+
 const { currentPage } = figma;
 
 const nodeTypes = [
@@ -30,10 +38,8 @@ const nodeTypes = [
 ] as const;
 
 export function parse_and_select(selector: string): void {
-  const parsed_node_types = parseNodeName(selector);
-  const nodes = figma.currentPage.findAllWithCriteria({
-    types: parsed_node_types,
-  });
+  console.log(JSON.stringify(parse_selector(selector)));
+  const nodes = find_selection(parse_selector(selector));
   currentPage.selection = nodes;
 }
 
@@ -46,4 +52,73 @@ function parseNodeName(input: string): SceneNodeType[] {
   } else {
     return [];
   }
+}
+
+function parse_selector(selector: string): Selector {
+  const parser = new CssSelectorParser();
+  parser.registerNestingOperators(">");
+  parser.registerSelectorPseudos("stuck");
+  return parser.parse(selector);
+}
+
+function find_selection(selector: Selector): SceneNode[] {
+  if (selector.type === "selectors") {
+    return selector.selectors.reduce((acc, ruleset) => {
+      const nodes = find_ruleset(ruleset);
+      return acc.concat(nodes);
+    }, [] as SceneNode[]);
+  } else {
+    return find_ruleset(selector);
+  }
+}
+
+function find_ruleset(ruleset: RuleSet): SceneNode[] {
+  return find_rule(ruleset.rule);
+}
+
+function find_rule(rule: Rule): SceneNode[] {
+  if (rule.tagName) {
+    const typesafe_tag_name = parseNodeName(rule.tagName.toUpperCase());
+    const all_typed_nodes = figma.currentPage.findAllWithCriteria({
+      types: typesafe_tag_name,
+    });
+    const attr_filtered_nodes = all_typed_nodes.filter((node) =>
+      matches_rule(node, rule)
+    );
+    return attr_filtered_nodes;
+  } else {
+    // TODO: handle no tag name
+    return [];
+  }
+}
+
+function matches_rule(node: SceneNode, rule: Rule): boolean {
+  // NOTE:
+  // We have to do this because the Rule type
+  // mistakenly has the attrs property as non-nullable,
+  // when in reality it is nullable
+  if ("attrs" in rule) {
+    for (const attr of rule.attrs) {
+      if (!matches_attr(node, attr)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function matches_attr(node: SceneNode, attr: RuleAttr): boolean {
+  if (attr.name === "name" && "value" in attr) {
+    return attr.value === node.name;
+  }
+  if (attr.name === "author" && "value" in attr) {
+    if (node.type === "STICKY") {
+      console.log(`author: ${attr.value} node.authorName: ${node.authorName}`);
+      return attr.value === node.authorName;
+    } else {
+      // Other node types don't have an author, so we don't match
+      return false;
+    }
+  }
+  return true;
 }
--- widget/src/code.tsx
+++ widget/src/code.tsx
@@ -1,35 +1,74 @@
 import { parse_and_select } from "figma-selector";
 
 const { widget } = figma;
-const { AutoLayout, Input, useSyncedState } = widget;
+const { Frame, Input, useSyncedState } = widget;
 
 function Selector() {
   const [text, setText] = useSyncedState("text", "");
   return (
-    <AutoLayout
+    <Frame
       name="Selector"
-      fill="#FFF"
-      cornerRadius={16}
-      spacing={10}
-      padding={{
-        vertical: 92,
-        horizontal: 132,
+      fill={{
+        type: "gradient-linear",
+        gradientHandlePositions: [
+          { x: 0, y: 0.5 },
+          { x: 1, y: 0.5 },
+          { x: 0, y: 0 },
+        ],
+        gradientStops: [
+          {
+            position: 0,
+            color: {
+              r: 0.6313725709915161,
+              g: 0.5490196347236633,
+              b: 0.8196078538894653,
+              a: 1,
+            },
+          },
+          {
+            position: 1,
+            color: {
+              r: 0.9843137264251709,
+              g: 0.7607843279838562,
+              b: 0.9215686321258545,
+              a: 1,
+            },
+          },
+        ],
       }}
+      cornerRadius={16}
+      width={600}
+      height={200}
     >
       <Input
         name="selector-input"
         fill="#000"
-        fontFamily="Inter"
+        fontFamily="Roboto Mono"
         fontSize={24}
-        onTextEditEnd={() => {}}
+        width={400}
+        horizontalAlignText="center"
+        x={{
+          type: "center",
+          offset: 0.5,
+        }}
+        y={{
+          type: "center",
+          offset: 0.5,
+        }}
+        onTextEditEnd={(event) => {
+          const text = event.characters;
+          setText(text);
+          parse_and_select(text);
+        }}
+        placeholder="css selector"
         value={text}
         inputFrameProps={{
-          fill: "#ddd",
+          fill: "#fff",
           cornerRadius: 16,
-          padding: 20,
+          padding: 8,
         }}
       ></Input>
-    </AutoLayout>
+    </Frame>
   );
 }
 
